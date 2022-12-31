f6376330bd51a14f2eb16b20cba8aedb5c0a0e3b
Daniel Suarez <suarez.daniel101@gmail.com>
2022-12-30 23:37:55 -0500

    Done.

---
+++ db/migrate/20221231025746_add_user_id_to_post_tag.rb
@@ -0,0 +1,5 @@
+class AddUserIdToPostTag < ActiveRecord::Migration[5.0]
+  def change
+    add_column :post_tags, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20221228030234) do
+ActiveRecord::Schema.define(version: 20221231030431) do
+
+  create_table "post_tags", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "tag_id"
+    t.integer  "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.text     "content"
   end
 
   create_table "tags", force: :cascade do |t|
