d084d7f716884bd20b18f23ba2dd1d0b4e3a5611
Daniel Suarez <suarez.daniel101@gmail.com>
2023-01-01 18:23:45 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,17 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :post_tags
   has_many :tags, through: :post_tags
-  
+
   validates :name, presence: true
   validates :content, presence: true
+#   accepts_nested_attributes_for :tags
+
+#   def tags_attributes=(tag_attributes)
+#     tag_attributes.values.each do |tag_attribute|
+#       tag = Tag.find_or_create_by(tag_attribute)
+#       self.post_tags.build(tag: tag)
+#     end
+#   end
 end
+
+
